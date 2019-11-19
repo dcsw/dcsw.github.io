71ea17f81b096e7407cb08aef51aac91fd5438e8
Dean Cirielli <dean.cirielli@mediavuesystems.com>
2019-11-19 09:19:07 -0500

    Localize media & add more

---
+++ setup.js
@@ -0,0 +1,101 @@
+var config = {
+  msecsInterval: 500,
+  urls: [
+    {
+      text: 'Decay -- Imagining reviving soil ecostructures beneath cities...',
+      url: './media/Decay.jpg'
+    },
+    {
+      text: 'Test Slides...',
+      url: './media/Test\\ Slides.jpg'
+    },
+    {
+      text: 'Generative Drawing',
+      url: './media/Generative\\ Drawing.jpg'
+    },
+    {
+      text: 'Modular Screen',
+      url: './media/Modular\\ Screen.jpg'
+    },
+    {
+      text: 'Acequia Erosion Module -- Creating modules to mitigate erosion on the Ditch system of the Rio Grande, New Mexico.',
+      url: './media/Acquea\\ Erosion\\ Modules  .jpg'
+    },
+    {
+      text: 'Ceramic cups and Eggshells dyed with Beet, Cabbage, and Turmeric dyes, set with vinegar.',
+      url: './media/Ceramic\\ Cups\\ and\\ Eggshells\\ vegetable\\ dyed.jpg'
+    },
+    {
+      text: 'Decay Digression',
+      url: './media/Decay\\ Digression.jpg'
+    },
+    {
+      text: 'Cradle',
+      url: './media/Cradle.jpg'
+    },
+    {
+      text: 'Decay Digression',
+      url: './media/Decay\\ Digression\\ 2.jpg'
+    },
+    {
+      text: 'Decay...',
+      url: './media/Decay....jpg'
+    },
+    {
+      text: 'Decay assembled...',
+      url: './media/Decay\\ assembled....jpg'
+    },
+    {
+      text: 'Rocketware launching!',
+      url: './media/Rocket\\ Bowls.jpg'
+    },
+    {
+      text: 'Rocketware experiments',
+      url: './media/Rocket\\ Bowls\\ green.jpg'
+    },
+    {
+      text: 'Basin gesture',
+      url: './media/Basin.jpg'
+    },
+    {
+      text: 'Microgreen growing table',
+      url: './media/Modular\\ Ceramic\\ \\&\\ Steel\\ Growing\\ Table.jpg'
+    },
+    {
+      text: 'Microgreen growing table',
+      url: './media/Modular\\ Ceramic\\ \\&\\ \\Steel\\ Growing\\ Table\\ Frame.jpg'
+    },
+    {
+      text: 'A form study in ceramic sculpture and food production, on symbiosis between growth and consumption',
+      url: './media/Cradle Full.jpg'
+    },
+    {
+      text: 'Air Plant Vase',
+      url: './media/Effervescent\\ Vase.jpg'
+    },
+    {
+      text: 'Texture',
+      url: './media/Lavaesque\\ Formations.jpg'
+    },
+    {
+      text: 'What would you drink out of these finned friends ?',
+      url: './media/Rocket\\ Cups\\ unglazed.jpg'
+    },
+    {
+      text: 'Take a fresh view on Ceramics',
+      url: './media/Window\\ on\\ Ceramics.jpg'
+    },
+    {
+      text: 'Plush Clay',
+      url: './media/Plush\\ Clay.jpg'
+    },
+    {
+      text: 'Microgreen Table viewed from below',
+      url: './media/Ceramic\\ \\&\\ Steel\\ Growing\\ Table\\ Underneath\\ View.jpg'
+    },
+    {
+      text: 'Microgreen Table',
+      url: './media/Ceramic\\ \\&\\ Steel\\ Growing\\ Table\\ Frame\\ Side\\ View.jpg'
+    }
+  ]
+};
